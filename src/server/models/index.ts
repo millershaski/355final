529cd2fe06839d7850596dba7a33f4add4783683
Tyler Millershaski <millershaski@gmail.com>
2025-04-23 16:28:03 -0400

    Removed some old files

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -1,9 +1,10 @@
 import {sequelize} from "../config/SequelizeInstance";
-import {Plant} from "./Old_Plant";
+
 
 const InitializeDatabase = () => 
 {
   return sequelize.sync(); 
 };
 
-export { sequelize, Plant, InitializeDatabase};
+
+export { sequelize, InitializeDatabase};
